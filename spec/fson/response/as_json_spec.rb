f36a6ed0f9a79579c2a61697e58ea666b5a4c4e7
Mateusz Kluczny <mateusz.kluczny@here.com>
2015-08-03 09:32:04 +0200

    redefined data builder method

--- spec/fson/response/as_json_spec.rb
+++ spec/fson/response/as_json_spec.rb
@@ -8,7 +8,7 @@ describe ::Fson::Response do
       # given
       response = ::Fson::Response.error
                      .add_error('invalid') { |e| e[:id] = 'text-set-1'}
-                     .data { |data| data << {:author => 'Mateusz Kluczny'} }
+                     .data_array { |data| data << {:author => 'Mateusz Kluczny'} }
 
       # when/then
       expect(response.as_json).to eq("{\"status\":\"error\",\"data\":[{\"author\":\"Mateusz Kluczny\"}],\"errors\":[{\"message\":\"invalid\",\"id\":\"text-set-1\"}]}")
